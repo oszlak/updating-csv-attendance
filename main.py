9480b402772e5813471fce9c18c2787babcba142
tomer oszlak <otak.site@gmail.com>
2022-02-05 23:46:12 +0200

    first time repository

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ FULL_DATA_FILE = "full_data.csv"
 
 
 def main():
+	#extracting the ids from the chat file
 	id_lst = id_sep.sep_id(CHAT_FILE)
+	#opening the csv file with all the names and ids of the class
 	with open(NAMES_FILE, encoding="utf-8") as f:
 		df = pd.read_csv(f)
 	for id_num in id_lst:
-		id_num_suffix = id_num[-4:-1]
 		df.at[df['תעודת זהות'].astype(str) == id_num, "נוכח"] = 'V'
 	write_to_file(df)
-	#print(df.get('נוכח'))
+
 def write_to_file(df):
 	df.to_csv(FULL_DATA_FILE, index=False, encoding='utf-8-sig')
 
